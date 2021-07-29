4972efd010f2ec8b6bc279b1b30705692496a224
Ali Maher <ali.maher.mohammed@gmail.com>
2021-07-29 06:44:27 +0200

    Finished lesson 9

---
+++ src/pages/about.js
@@ -0,0 +1,32 @@
+import React from "react"
+import Layout from "../components/Layout"
+
+const About = () => {
+  return (
+    <Layout>
+      <div>
+        <h1>About Page</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod tempore
+          magnam nulla vero ad itaque laudantium laborum hic delectus! Dolorem
+          eius deserunt at provident. Tempore laudantium molestias vitae odit
+          ea!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod tempore
+          magnam nulla vero ad itaque laudantium laborum hic delectus! Dolorem
+          eius deserunt at provident. Tempore laudantium molestias vitae odit
+          ea!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod tempore
+          magnam nulla vero ad itaque laudantium laborum hic delectus! Dolorem
+          eius deserunt at provident. Tempore laudantium molestias vitae odit
+          ea!
+        </p>
+      </div>
+    </Layout>
+  )
+}
+
+export default About
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,39 @@
+import { graphql, Link } from "gatsby"
 import React from "react"
+import Layout from "../components/Layout"
+import * as styles from "../styles/home.module.css"
 
-export default function Home() {
-  return <div>Hello world!</div>
+const Home = ({ data }) => {
+  const { title, description } = data.site.siteMetadata
+  return (
+    <Layout>
+      <section className={styles.header}>
+        <div>
+          <h2>Design</h2>
+          <h3>Develop & Deploy</h3>
+          <p>UX designer & web developer</p>
+          <Link className={styles.btn} to="/projects">
+            My Portfolio Projects
+          </Link>
+        </div>
+        <img src="/banner.png" alt="banner" style={{ maxWidth: "100%" }} />
+        <p>
+          {title} - {description}
+        </p>
+      </section>
+    </Layout>
+  )
 }
+
+export default Home
+
+export const query = graphql`
+  query SiteInfo {
+    site {
+      siteMetadata {
+        description
+        title
+      }
+    }
+  }
+`
